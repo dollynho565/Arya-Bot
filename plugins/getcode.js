d200ef8e66f6641058a1d24bfbb2463ee46624e3
dollynho565 <77021968+dollynho565@users.noreply.github.com>
2021-01-12 18:27:10 -0300

    Update getcode.js

--- plugins/getcode.js
+++ plugins/getcode.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix }) => {
-  if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'Command ini hanya untuk yang jadi bot', m)
+  if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'O BOT NAO E ADM SEU ANIMAL, TA ACHANDO QUE SOU MAGICO?', m)
   else global.conn.reply(conn.user.jid, `${usedPrefix}jadibot ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, m)
 }
 handler.command = /^(getcode)$/i
